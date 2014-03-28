0d4df31eba5059e74ffe20514a5f7b70ef86ac44
cankoluman <n2dbrx@gmail.com>
2014-03-28 14:50:36 +0000

    Initial set-up for the .NET 4.5 build config

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 set msbuild40=%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
 %msbuild40% /m build.3.5.xml
 %msbuild40% /m build.4.0.xml
+%msbuild40% /m build.4.5.xml
 %msbuild40% /m build.package.xml
